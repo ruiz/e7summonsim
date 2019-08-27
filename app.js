4d9de116bf68b44041c60f0e0d0dfe6222f1b64d
Ziqi Rui <zrui@uwo.ca>
2019-08-26 22:29:42 -0400

    styling, container css changes

--- app.js
+++ app.js
@@ -2,20 +2,6 @@ function summonGach(){
     console.log("test");
 }
 
-function openCity(evt, cityName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("city");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" w3-red", ""); 
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " w3-red";
-}
-
 function getSummonInfo(evt, summonType){
     var i, x, tablinks;
     x = document.getElementsByClassName("summon");
@@ -26,6 +12,6 @@ function getSummonInfo(evt, summonType){
     for(i=0; i<x.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" w3-red","");
     }
-    document.getElementById(summonType).style.display = "table";
+    document.getElementById(summonType).style.display = "block";
     evt.currentTarget.className += " w3-red";
 }
