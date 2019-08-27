c8d2b4e3969b2907aabc3c6081e8d8c9aab2b8d8
Ziqi Rui <zrui@uwo.ca>
2019-08-26 22:00:26 -0400

    new styling, tabs, etc

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 function summonGach(){
     console.log("test");
 }
+
+function openCity(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", ""); 
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
+
+function getSummonInfo(evt, summonType){
+    var i, x, tablinks;
+    x = document.getElementsByClassName("summon");
+    for(i=0; i<x.length; i++){
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for(i=0; i<x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-red","");
+    }
+    document.getElementById(summonType).style.display = "table";
+    evt.currentTarget.className += " w3-red";
+}
