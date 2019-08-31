f8df6e1baad691b32215d9edc8cb830d7bc95a10
Ziqi Rui <zrui@uwo.ca>
2019-08-31 16:58:02 -0400

    commit

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ function getSummonInfo(evt, summonType){
     }
     tablinks = document.getElementsByClassName("tablink");
     for(i=0; i<x.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" w3-red","");
+        tablinks[i].className = tablinks[i].className.replace(" highLight","");
     }
     document.getElementById(summonType).style.display = "block";
-    evt.currentTarget.className += " w3-red";
+    evt.currentTarget.className += " highLight";
 }
