18977cbbeff5be87750c552ec2558dc740371eef
Ziqi Rui <zrui@uwo.ca>
2019-09-16 22:31:53 -0400

    adding caching, db unit retrieval, summoning support

--- app.js
+++ app.js
@@ -3,10 +3,56 @@ const dbURL = "https://epicsevendb.com/";
 const assetsURL = "https://assets.epicsevendb.com/";
 const randomURL = "https://www.random.org/";
 
-var assetCache
+var cardList = [];
+var unitsAcquired = 0;
+
+var unitCache = {
+    hero: {loaded: false},
+    artifact: {loaded: false},
+}
+
+function cacheUnits(type){
+    $.get(dbAPI + type, function(data, status){
+        //alert(status);
+        if(status == "success"){
+            var units = data.results;
+            $.each(units, function(i){
+                unitCache[type][units[i].fileId] = units[i];
+            });
+            unitCache[type].loaded = true;
+        }
+    });
+}
+
+function loadUnits(){
+    cacheUnits("hero");
+    cacheUnits("artifact");
+}
+
+loadUnits();
+
+function summonUnit(type, fileId, idx){
+    var unit = unitCache[type][fileId];
+    var $card = cardList[idx - 1];
+    var unitImage = assetsURL + type + "/" + fileId + "/small" + getImgExtension(type);
+
+}
+
+function getImgExtension(type){
+    if(type == "hero") return ".png";
+    return ".jpg";
+}
 
 var $summon_grid = $(".summon-grid").isotope({
+    itemSelector: ".summon-card"
+});
 
+$("#summon").click(function(){
+    var type = Math.random()<0.5 ? "hero" : "artifact";
+    var randUnit = Object.keys(unitCache[type]);
+    var fileId =  unitCache[type][randUnit[randUnit.length * Math.random()<<0]].fileId;
+    unitsAcquired++;
+    summonUnit(type, fileId, unitsAcquired);
 });
 
 $(".banner-item").click(function(){
